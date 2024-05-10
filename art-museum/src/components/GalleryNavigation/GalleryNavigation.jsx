a814672739be49eb43cebd908d3a322d41943fbc
CharLee Racine <charleeracine@yahoo.com>
2024-05-10 14:18:23 -0600

    next is add navlink

---
+++ art-museum/src/components/GalleryNavigation/GalleryNavigation.jsx
@@ -0,0 +1,10 @@
+
+function GalleryNavigation(props){
+    const galleries = props.galleries
+    debugger;    
+    return(
+        <h1>Galleries</h1>
+    )
+}
+
+export default GalleryNavigation;
